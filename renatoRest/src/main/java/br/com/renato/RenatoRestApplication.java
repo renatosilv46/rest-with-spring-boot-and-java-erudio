03dc215ccd40a7f32402253b6c8d1a2b1b6aa6b1
Renato <rnt.graca01@gmail.com>
2023-12-10 12:28:53 -0300

    Creating Controllers of end points with Java and Spring Boot

---
+++ renatoRest/src/main/java/br/com/renato/RenatoRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.renato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenatoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenatoRestApplication.class, args);
+	}
+
+}
---
+++ renatoRest/src/test/java/br/com/renato/RenatoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenatoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
